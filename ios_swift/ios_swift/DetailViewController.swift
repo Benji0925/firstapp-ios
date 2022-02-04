f0de93347a28477796e98ec0376f23e5a96b913e
Binyam Assefa <binia6181@gmail.com>
2022-02-03 20:02:47 -0600

    Asked people where they go

---
+++ ios_swift/ios_swift/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ios_swift
+//
+//  Created by JSU Makerspace  on 9/13/19.
+//  Copyright © 2019 JSU Makerspace . All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
